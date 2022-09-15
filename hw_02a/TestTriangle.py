b23f71d223c4393f6f2c3e5dad225fde76abdb81
sanjanamadhu <sanjana.s.madhu@gmail.com>
2022-09-15 11:13:10 -0400

    added tests

--- hw_02a/TestTriangle.py
+++ hw_02a/TestTriangle.py
@@ -22,9 +22,49 @@ class TestTriangles(unittest.TestCase):
 
     def testRightTriangleB(self): 
         self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+
+    def testRightTriangleC(self): 
+        self.assertEqual(classifyTriangle(8,15,17),'Right','8,15,17 is a Right triangle')
+
         
-    def testEquilateralTriangles(self): 
+    def testEquilateralTriangleA(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+
+    def testEquilateralTriangleB(self): 
+        self.assertEqual(classifyTriangle(10,10,10),'Equilateral','10,10,10 should be equilateral')
+
+    def testEquilateralTriangleC(self): 
+        self.assertEqual(classifyTriangle(12,12,12),'Equilateral','12,12,12 should be equilateral')
+
+
+    def testIsocelesTriangleA(self): 
+        self.assertEqual(classifyTriangle(6,6,8),'Isoceles','6,6,8 is a Isoceles triangle')
+
+    def testIsocelesTriangleB(self): 
+        self.assertEqual(classifyTriangle(4,6,6),'Isoceles','4,6,6 is a Isoceles triangle')
+
+    def testIsocelesTriangleC(self): 
+        self.assertEqual(classifyTriangle(21,14,14),'Isoceles','21,14,14 is a Isoceles triangle')
+
+
+    def testScaleneTriangleA(self): 
+        self.assertEqual(classifyTriangle(4,6,8),'Scalene','4,6,8 is a Scalene triangle')
+
+    def testScaleneTriangleB(self): 
+        self.assertEqual(classifyTriangle(15,34,32),'Scalene','15,34,32 is a Scalene triangle')
+
+    def testScaleneTriangleC(self): 
+        self.assertEqual(classifyTriangle(21,14,12),'Scalene','21,14,12 is a Scalene triangle')
+
+
+    def testNotATriangleTriangleA(self): 
+        self.assertEqual(classifyTriangle(4,5,10),'NotATriangle','4,5,10 is NotATriangle')
+
+    def testNotATriangleTriangleB(self): 
+        self.assertEqual(classifyTriangle(4,4,8),'NotATriangle','4,4,8 is NotATriangle')
+
+    def testNotATriangleTriangleC(self): 
+        self.assertEqual(classifyTriangle(2,2,19),'NotATriangle','21,12,14 is a triangle')
 
 if __name__ == '__main__':
     print('Running unit tests')
